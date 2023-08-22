d4f06adb6ef0cac62d4aef2173abd7249048f7bd
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-08-22 03:58:53 +0000

    [Add]ヘッダー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,32 @@
 class BooksController < ApplicationController
-  def new
+  def show
     @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path(@book)
+    else
+      render :index
+    end
   end
 
   def index
     @books = Book.all
   end
 
-
-  def show
+  def edit
     @book = Book.find(params[:id])
   end
 
-
-  def edit
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
